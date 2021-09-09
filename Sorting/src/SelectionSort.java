f0b04198790c7b3f967fdac355813f3bbc12a214
Rikshit20 <90380799+Rikshit20@users.noreply.github.com>
2021-09-09 14:11:56 +0530

    Add files via upload

---
+++ Sorting/src/SelectionSort.java
@@ -0,0 +1,30 @@
+
+public class SelectionSort 
+{
+	public static void main(String[] args) 
+	{
+	     int array[] = {10, 20, 25, 60, 90, 50};
+	      int size = array.length;
+
+	      for (int i = 0 ;i< size-1; i++)
+	      {
+	         int min = i;
+
+	         for (int j = i+1; j<size; j++)
+	         {
+	            if (array[j] < array[min])
+	            {
+	            min = j;
+	            }
+	         }
+	         int temp = array[min];
+	         array[min] = array[i];
+	         array[i] = temp;
+	      }
+
+	      for (int i = 0 ;i< size; i++)
+	      {
+	         System.out.print(" "+array[i]);
+	      }
+	 } 
+}
